9272a654201f08e1b8607228567843a06b81696f
Elsa Duarte <elsasantosduarte@gmail.com>
2022-04-25 17:16:49 -0100

    Amélioration de la correction de fileReader

--- projet/bin/main.ml
+++ projet/bin/main.ml
@@ -2,4 +2,4 @@ open FileTreatment;;
 
 let () =
     print_endline "Hello, World!";;
-    print_file "projet/bin/FileReader/exemples/fichier1.txt";;
+    print_file "projet/exemples/etape1/date_to_month";;
