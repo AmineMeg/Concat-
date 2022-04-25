f25f08ca79a3d9becadd4a98ffe22c6d2e037987
amine <meguenni@informatique.univ-paris-diderot.fr>
2022-04-25 13:28:17 +0200

    Ajout de la lecture de fichiers ligne par ligne avec option de choisir quelle fonction appliquer a chaque ligne (dans main.ml)

--- projet/bin/AST/AST.ml
+++ projet/bin/AST/AST.ml
@@ -5,8 +5,10 @@ type program = expression located list
 
 and expression =
 |const of string
-|extract pos_expression * pos_expression
+|extract of pos_expression * pos_expression
 
 and pos_expression =
 |forward of int
 |backward of int
+
+
